2d3dfa7ef239d64e577689773b9f5809ebc39364
Abdullah-Muhammed <abdo56668@gamil.com>
2019-09-12 14:02:06 +0200

    forgot password - https://tfs.ibtikar.sa/tfs/Ibtikar-Projects/iti-training/_workitems?id=49728&_a=edit - reset password with phone and make link expire in 24 hours - modify email text (not done)

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -11,7 +11,7 @@
         @csrf
 
         <div class="form-group">
-            <input id="email" type="email" placeholder="{{ __('E-Mail Address') }}" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <input id="email" type="text" placeholder="{{ __('E-Mail Address') }}" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
             @error('email')
                 <span class="invalid-feedback" role="alert">
